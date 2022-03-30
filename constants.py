dc306398e28de9fd3f0d39ea872d6c2dd8ea6cd1
NEVI0 <neviocostamagagnin@gmail.com>
2022-03-29 22:02:19 -0300

    Initial commit

---
+++ constants.py
@@ -0,0 +1,25 @@
+from cv2 import FONT_HERSHEY_SIMPLEX
+
+WINDOW = {
+    'NAME': 'Parking lots'
+}
+
+PARKING = {
+    'WIDTH': 107,
+    'HEIGHT': 48
+}
+
+SPACE_COLORS = {
+    'AVAILABLE': (74, 242, 41),
+    'NOT_AVAILABLE': (24, 24, 244)
+}
+
+TEXT = {
+    'FONT': FONT_HERSHEY_SIMPLEX,
+    'SIZE': 2,
+    'SCALE': 1,
+    'COLORS': {
+        'RED': (24, 24, 244),
+        'BLUE': (122, 64, 14)
+    }
+}
